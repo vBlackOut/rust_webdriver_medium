f02df9c841c5eb7bc3e844b06940a9d7dbaa4dd3
vBlackOut <vladimir.souchet@cuby-hebergs.com>
2025-02-25 15:15:27 +0100

    Add comment for explain code

--- src/main.rs
+++ src/main.rs
@@ -10,28 +10,33 @@ async fn get_page(driver: &WebDriver) -> Result<(), Box<dyn Error + Send + Sync>
 }
 
 async fn get_element_text_xpath(driver: &WebDriver, xpath_element: &str, author: &str) -> Result<(), Box<dyn Error + Send + Sync>> {
-    let _element = driver.find_all(By::XPath(xpath_element)).await?; // Sélectionne le premier <h1>
-    //dbg!(_element.html().await?);
-    //let text = element.text().await?;
-    //println!("Texte trouvé: {}", text);
+    let _element = driver.find_all(By::XPath(xpath_element)).await?;
+    
     println!("Nombre d'éléments trouvés : {}", _element.len());
 
     let mut links_set = HashSet::new();
-
+    // iter for all parent element
     for (i, item) in _element.iter().enumerate() {
         let text_html = item.text().await?;
 
-        // find author in page
+        // find link author in parent (this is search child) 
         let _element2 = item.find_all(By::XPath(author)).await?;
-        for (i, item2) in _element2.iter().enumerate() {
-          let link = &item2.attr("href").await?;
 
+        // iter child element
+        for (i, item2) in _element2.iter().enumerate() {
+          // give attrib link
+          let link = &item2.attr("href").await?;
+            
           if let Some(link) = link {
                let link_parts: Vec<&str> = link.split("/").collect();
-
+              
+               // compare is contains author in url @....
                if link_parts[1].contains("@") {
                     let url_user: Vec<_> = link_parts[1].split("?").collect();
+                    // build url
                     let formatted_link = format!("https://medium.com/{}/feed", url_user[0]);
+                    
+                    // inject url in Set list.
                     links_set.insert(formatted_link);
                }
           } else {
@@ -40,10 +45,12 @@ async fn get_element_text_xpath(driver: &WebDriver, xpath_element: &str, author:
         }
 
     }
-
+    
+    // print output set list
     for link in &links_set {
         println!("{}", link);
     }
+    
     Ok(())
 }
 
@@ -51,9 +58,8 @@ async fn get_element_text_xpath(driver: &WebDriver, xpath_element: &str, author:
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     // define the browser options
     let caps = DesiredCapabilities::chrome();
-
-    // initialize a driver to control a Chrome instance
-    // with the specified options
+    
+    // launch chromdriver 
     let driver = WebDriver::new("http://localhost:34997", caps).await?;
 
     // visit the target page
@@ -63,24 +69,31 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
 
     let link_search = format!("{}/{}", "https://medium.com", search);
     driver.goto(link_search).await?;
-    // retrieve the source HTML of the target page
-    // and print it
+
     for i in 1..=number_scroll {
      driver.execute_script("window.scrollTo(0, document.body.scrollHeight);", vec![]).await?;
+     // wait minor time for detect button after every loop for
      sleep(Duration::from_millis(timesleep_scroll)).await;
+     
+     // find element button "Show more"
      match driver.find(By::XPath("//button[contains(text(), 'Show more')]")).await {
           Ok(button) => {
+               // if exist click on button for show more information...
                println!("Bouton trouvé avec XPath, clique numéro: {}", i);
                button.click().await?;
           }
           Err(_) => println!("Bouton introuvable"),
      }
     }
-
+    
+    // get full page sources
     // get_page(&driver).await?;
+    
+    // define xpath if it's page or articles search.
     let page = "//div[@id='root']/div/div[3]/div[2]/div/main/div/div[2]/div";
     let search = "//div[@id='root']/div/div[3]/div[2]/div/main/div/div/div[2]/div";
-
+    
+    // function for give parent element and give the children element extract link.
     get_element_text_xpath(&driver, search, "//a[@rel='noopener follow']").await?;
 
     // close the browser and release its resources
